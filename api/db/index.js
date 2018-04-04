b90146b6a60b95109a7c945d15313318f38c2459
Dergunov Dmitry <dmitry@dergunov.net>
2018-04-04 15:14:32 +0400

    connect to DB with EnvVar

--- api/db/index.js
+++ api/db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/daily-spasibo');
+console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DB}`);
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DB}`);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
