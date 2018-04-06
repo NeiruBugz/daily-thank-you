577971ef3ebbf02730c096f726152cdc866a09d8
Dergunov Dmitry <dmitry@dergunov.net>
2018-04-06 04:18:05 +0400

    fix generate url on oauth

--- api/oauth/index.js
+++ api/oauth/index.js
@@ -1,17 +1,14 @@
 const {google} = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
-// https://spasibo.dergunov.net/?code=4/AABJiRrKxG_RVC8cgqpGL-KqXNH-riD4qlo0EYQDBuzPqeT-Bi22p3ete5ctw4kODgmVKra-q0F2h-3Iu02jwwM#
 const oauth2Client = new OAuth2(
   '189576862785-p83qioo93omueb6788uhm27dtfbbugvm.apps.googleusercontent.com',
   'Y7bz9Rx7fp0ojl5l2avXIqpb',
-  'https://spasibo.dergunov.net'
+  'https://spasibo-spasibo.7e14.starter-us-west-2.openshiftapps.com/oauth/validate'
 );
 
 function generateAuthUrl() {
   return oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: 'offline',
     // If you only need one scope you can pass it as a string
     scope: 'https://www.googleapis.com/auth/plus.me',
   });
--- api/routes/oauth.js
+++ api/routes/oauth.js
@@ -2,15 +2,9 @@ const express = require('express');
 const router = express.Router();
 const oauth = require('../oauth/index');
 
-/* GET user by ID. */
+/* GET auth URL. */
 router.get('/', (req, res) => {
-  const response = oauth.generateAuthUrl();
-  response
-    .then(user => res.send(JSON.stringify(user)))
-    .catch(err => {
-      console.error(err);
-      res.status(404).send('Not Found');
-    });
+  res.redirect(oauth.generateAuthUrl());
 });
 
 module.exports = router;
