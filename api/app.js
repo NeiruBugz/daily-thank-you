41cb909d44da2cb6ad93089123244971340ec394
Dergunov Dmitry <dmitry@dergunov.net>
2018-04-12 17:26:42 +0400

    config cors

--- api/app.js
+++ api/app.js
@@ -14,6 +14,15 @@ app.use('/oauth', oauth);
 app.use('/users', users);
 app.use('/spasibo', spasibo);
 
+// set CORS config and forward to 404 handler
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
