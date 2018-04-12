ec031f25269142f604805288124f6cdb712aa0fa
Nail Badiullin <badiullin.nail@gmail.com>
2018-04-12 19:25:50 +0400

    add react-router && routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login/Login';
-import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile'
 import ReadThank from './components/ReadThank/ReadThank';
 
 export default class App extends Component {
   render() {
     return (
-      <Profile/>
+      <Router>
+        <div>
+          <Route path='/' exact component={Login}/>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/readthank' component={ReadThank}/>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import Header from '../Header/Header'
-import { ProfileWrapper, UserInfo, UserImage, UserName, UserEmail, UserContent } from './styles';
+import {
+  ProfileWrapper,
+  UserInfo,
+  UserImage,
+  UserName,
+  UserEmail,
+  UserContent
+} from './styles';
 
+const Store = require('../../containers/Store');
 
 export default class Profile extends React.Component {
+  getUserData = () => {
+    Store
+      .getInfo()
+      .then(user => {
+        this.setState({user: user});
+      })
+  }
 
   render() {
+    require('../../containers/Store');
     return (
       <ProfileWrapper>
         <Header/>
         <UserInfo>
-          <UserImage src='https://www.w3schools.com/howto/img_avatar.png' alt='User image'/>
+          <UserImage src='' alt='User image'/>
           <UserContent>
-            <UserName>Имя пользователя</UserName>
-            <UserEmail>Email пользователя</UserEmail>
+            <UserName>''</UserName>
+            <UserEmail>''</UserEmail>
           </UserContent>
         </UserInfo>
       </ProfileWrapper>
