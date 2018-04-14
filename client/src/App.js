c0648dc8ba2088cc210902dcdaafd862e305334a
Dergunov Dmitry <dmitry@dergunov.net>
2018-04-14 14:35:58 +0400

    add privacy policy

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login/Login';
 import Profile from './components/Profile/Profile'
 import ReadThank from './components/ReadThank/ReadThank';
+import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class App extends Component {
           <Route path='/' exact component={Login}/>
           <Route path='/profile' component={Profile}/>
           <Route path='/readthank' component={ReadThank}/>
+          <Route path='/privacy-policy' component={PrivacyPolicy}/>
         </div>
       </Router>
     );
---
+++ client/src/components/PrivacyPolicy/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Article = styled.main `
+  padding: 1rem 2rem;
+  text-align: justify;
+`;
