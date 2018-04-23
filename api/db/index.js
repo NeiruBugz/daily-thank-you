cf9a347a397f847061319e1522d4ef10c604b950
Dergunov Dmitry <dmitry@dergunov.net>
2018-04-24 02:35:15 +0400

    fix db error

--- api/db/index.js
+++ api/db/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DB}`);
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DB}`);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
